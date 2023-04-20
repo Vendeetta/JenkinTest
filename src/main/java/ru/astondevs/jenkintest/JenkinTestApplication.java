29d7df265db4bc13551d447aa167ae2d8fc14d22
Vendeetta <vendeetta@gmail.com>
2023-04-20 11:06:29 +0300

    POM change

--- src/main/java/ru/astondevs/jenkintest/JenkinTestApplication.java
+++ src/main/java/ru/astondevs/jenkintest/JenkinTestApplication.java
@@ -2,9 +2,10 @@ package ru.astondevs.jenkintest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinTestApplication {
+public class JenkinTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinTestApplication.class, args);
--- src/main/java/ru/astondevs/jenkintest/web/HelloWorldController.java
+++ src/main/java/ru/astondevs/jenkintest/web/HelloWorldController.java
@@ -1,15 +1,20 @@
 package ru.astondevs.jenkintest.web;
 
+import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Level;
+
 @RestController()
 @RequestMapping("/hello")
+@Log
 public class HelloWorldController {
 
     @GetMapping
     public String hello() {
+        log.log(Level.WARNING, "We are here.");
         return "Hello World!";
     }
 }
